e4f5c1021ab1f93cb128a160900c5b636dcb426a
woosupyun <yunwoosup@naver.com>
2020-09-15 02:18:33 +0900

    myBatis 스프링 연동(java 설정)

--- jex00/src/main/java/org/zerock/config/RootConfig.java
+++ jex00/src/main/java/org/zerock/config/RootConfig.java
@@ -2,6 +2,8 @@ package org.zerock.config;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -24,5 +26,12 @@ public class RootConfig {
 			
 			HikariDataSource dataSource = new HikariDataSource(hikariConfig);
 			return dataSource;
+		}
+		
+		@Bean
+		public SqlSessionFactory sqlSessionFactory() throws Exception{
+			SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+			sqlSessionFactory.setDataSource(dataSource());
+			return (SqlSessionFactory) sqlSessionFactory.getObject();
 		}
 }
--- jex00/src/test/java/org/zerock/persistence/DataSourceTests.java
+++ jex00/src/test/java/org/zerock/persistence/DataSourceTests.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,9 +26,15 @@ public class DataSourceTests {
 	@Setter(onMethod_ = {@Autowired})
 	private DataSource dataSource;
 	
+	@Setter(onMethod_ = {@Autowired})
+	private SqlSessionFactory sqlSessionFactory;
+	
 	@Test
 	public void testConnection() {
-		try(Connection con = dataSource.getConnection()){
+		try(SqlSession session = sqlSessionFactory.openSession();
+			Connection con = dataSource.getConnection();){
+
+			log.info(session);
 			log.info(con);
 		}catch(Exception e) {
 			fail(e.getMessage());
