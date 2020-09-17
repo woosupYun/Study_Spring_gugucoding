22dbb218360ae23a407fa6567e583c53932400eb
woosupyun <yunwoosup@naver.com>
2020-09-18 04:03:12 +0900

    GetMapping 사용하기

--- ex00/src/main/java/org/zerock/controller/SampleController.java
+++ ex00/src/main/java/org/zerock/controller/SampleController.java
@@ -1,20 +1,24 @@
 package org.zerock.controller;
 
+import java.text.SimpleDateFormat;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.zerock.domain.SampleDTO;
+import org.zerock.domain.SampleDTOList;
 
 import lombok.extern.log4j.Log4j;
 
 @Controller
-//@RequestMapping("/sample/*")
+@RequestMapping("/sample/*")
 @Log4j
 public class SampleController {
-//	@RequestMapping("")
-//	public void basic() {
-//		log.info("basic123...............");
-//	}
+	@RequestMapping("")
+	public void basic() {
+		log.info("basic123...............");
+	}
 
 	@RequestMapping(value="/basic", method =RequestMethod.POST)
 	public void basicGet1() {
@@ -25,4 +29,18 @@ public class SampleController {
 	public void basicGet2() {
 		log.info("basic get only get ...................");
 	}
+	
+	@GetMapping("/ex01")
+	public String ex01(SampleDTO dto) {
+		log.info("" + dto);
+		return "ex01";
+	}
+	
+	@GetMapping("/ex02Bean")
+	public String ex02Bean(SampleDTOList list) {
+		log.info("list dtos: " + list);
+		
+		return "ex02Bean";
+	}
+	
 }
