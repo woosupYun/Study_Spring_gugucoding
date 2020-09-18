85bee77bc4cea33314fc95656651c346b48030bb
woosupyun <yunwoosup@naver.com>
2020-09-18 21:41:42 +0900

    Controller 리턴 type에 따른 처리 (json 형식으로 리턴하기 test)

---
+++ ex00/src/main/java/org/zerock/domain/SampleDTOList.java
@@ -0,0 +1,13 @@
+package org.zerock.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SampleDTOList {
+	
+	private List<SampleDTO> list;
+	
+	public SampleDTOList() {
+		list = new ArrayList<>();
+	}
+}
