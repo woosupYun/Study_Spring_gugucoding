a3573e2bd5af7b011571f8ecfc32abc0192e7345
woosupyun <yunwoosup@naver.com>
2020-09-18 21:32:59 +0900

    Cotroller 리턴 type 에 따른 처리 (json데이터 만들기)

--- ex00/src/main/java/org/zerock/controller/SampleController.java
+++ ex00/src/main/java/org/zerock/controller/SampleController.java
@@ -1,11 +1,11 @@
 package org.zerock.controller;
 
-import java.text.SimpleDateFormat;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.zerock.domain.SampleDTO;
 import org.zerock.domain.SampleDTOList;
 
@@ -43,4 +43,31 @@ public class SampleController {
 		return "ex02Bean";
 	}
 	
+	
+	@GetMapping("/ex04")
+	public String ex04(SampleDTO dto, int page) {
+		log.info("dto: " +dto);
+		log.info("page: "+page);
+		
+		return "/sample/ex04";
+	}
+	
+	@GetMapping("/ex05")
+	public String ex05(SampleDTO dto, @ModelAttribute("page")int page) {
+		log.info("dto: " +dto);
+		log.info("page: "+page);
+		
+		return "/sample/ex04";
+	}
+	
+	@GetMapping("/ex06")
+	public @ResponseBody SampleDTO ex06() {
+		log.info("/ex06................");
+		
+		SampleDTO dto = new SampleDTO();
+		dto.setAge(10);
+		dto.setName("홍길동");
+		
+		return dto;
+	}
 }
